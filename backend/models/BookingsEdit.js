2fb5e2951a4c95cfef3dc70307f0064ad6481d7f
bhagyesh452 <bhagyesh@startupsahay.com>
2024-02-15 12:54:57 +0530

    Location Removed

---
+++ backend/models/BookingsEdit.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const leadSchema = new mongoose.Schema({
+
+    bdeName: {
+        type:String,
+       
+    },
+    bdeEmail:{
+        type:String,
+    },
+    bdmName: {
+        type:String,
+    },
+    bdmEmail:{
+        type:String,
+    },
+    bdmType: String,
+    supportedBy: Boolean,
+    bookingDate: Date,
+    caCase: String,
+    caNumber: Number,
+    caEmail: String,
+    caCommission: String,
+    companyName: String,
+    contactNumber: Number,
+    companyEmail: String,
+    services: Array,
+    originalTotalPayment: Number,
+    totalPayment: Number,
+    paymentTerms: String,
+    paymentMethod: String,
+    firstPayment: Number,
+    secondPayment: Number,
+    thirdPayment: Number,
+    fourthPayment: Number,
+    paymentRemarks:String,
+    paymentReceipt: String,
+    bookingSource: String,
+    cPANorGSTnum: String,
+    incoDate: Date,
+    extraNotes: String,
+    otherDocs: Array,
+    bookingTime: String
+});
+
+const BookingsRequestModel = mongoose.model('BookingEditRequests', leadSchema);
+
+module.exports = BookingsRequestModel;
