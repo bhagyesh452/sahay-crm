47ab430eea3533ff65b307311823b859654359bb
shivangistartupsahay <shivangi@startupsahay.com>
2024-04-08 14:19:07 +0530

    08-04-2024

---
+++ frontend/src/BDM/Dashboard/BdmDashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useParams } from "react-router-dom";
+
+function BdmDashboard() {
+    const { userId } = useParams();
+
+
+
+
+
+
+
+  return (
+    <div>BdmDashboard</div>
+  )
+}
+
+export default BdmDashboard
