b1f0a46a9b7079d825d194b3b12a10635d9cc4f2
bhagyesh452 <bhagyesh@startupsahay.com>
2024-04-18 14:23:55 +0530

    boom

--- frontend/src/employeeComp/EmployeePanel.js
+++ frontend/src/employeeComp/EmployeePanel.js
@@ -2013,6 +2013,82 @@ function EmployeePanel() {
   const [forwardedCompany, setForwardedCompany] = useState("");
   const [bdmNewAcceptStatus, setBdmNewAcceptStatus] = useState("");
   const [forwardCompanyId, setforwardCompanyId] = useState("");
+  const [feedbakPoints, setFeedbackPoints] = useState("")
+  const [feedbackRemarks, setFeedbackRemarks] = useState("")
+  const [feedbackPopupOpen, setFeedbackPopupOpen] = useState(false)
+  const [feedbackCompany, setFeedbackCompany] = useState("")
+  const handleViewFeedback = (companyId, companyName, companyFeedbackRemarks, companyFeedbackPoints) => {
+    setFeedbackPopupOpen(true)
+    setFeedbackPoints(companyFeedbackPoints)
+    setFeedbackRemarks(companyFeedbackRemarks)
+    setFeedbackCompany(companyName)
+  }
+  const closeFeedbackPopup = () => {
+    setFeedbackPopupOpen(false)
+  }
+  function ValueLabelComponent(props) {
+    const { children, value } = props;
+
+    return (
+      <Tooltip enterTouchDelay={0} placement="top" title={value}>
+        {children}
+      </Tooltip>
+    );
+  }
+
+  ValueLabelComponent.propTypes = {
+    children: PropTypes.element.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+  const iOSBoxShadow =
+    '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+
+  const IOSSlider = styled(Slider)(({ theme }) => ({
+    color: theme.palette.mode === 'dark' ? '#0a84ff' : '#007bff',
+    height: 5,
+    padding: '15px 0',
+    '& .MuiSlider-thumb': {
+      height: 20,
+      width: 20,
+      backgroundColor: '#fff',
+      boxShadow: '0 0 2px 0px rgba(0, 0, 0, 0.1)',
+      '&:focus, &:hover, &.Mui-active': {
+        boxShadow: '0px 0px 3px 1px rgba(0, 0, 0, 0.1)',
+        // Reset on touch devices, it doesn't add specificity
+        '@media (hover: none)': {
+          boxShadow: iOSBoxShadow,
+        },
+      },
+      '&:before': {
+        boxShadow:
+          '0px 0px 1px 0px rgba(0,0,0,0.2), 0px 0px 0px 0px rgba(0,0,0,0.14), 0px 0px 1px 0px rgba(0,0,0,0.12)',
+      },
+    },
+    '& .MuiSlider-valueLabel': {
+      fontSize: 12,
+      fontWeight: 'normal',
+      top: -6,
+      backgroundColor: 'unset',
+      color: theme.palette.text.primary,
+      '&::before': {
+        display: 'none',
+      },
+      '& *': {
+        background: 'transparent',
+        color: theme.palette.mode === 'dark' ? '#fff' : '#000',
+      },
+    },
+    '& .MuiSlider-track': {
+      border: 'none',
+      height: 5,
+    },
+    '& .MuiSlider-rail': {
+      opacity: 0.5,
+      boxShadow: 'inset 0px 0px 4px -2px #000',
+      backgroundColor: '#d0d0d0',
+    },
+  }));
 
   // const handleConfirmAssign = (companyId, companyName, companyStatus, ename, bdmAcceptStatus) => {
   //   console.log(companyName, companyStatus, ename, bdmAcceptStatus, companyId);
@@ -3700,6 +3776,7 @@ function EmployeePanel() {
                                     <td>
                                       <IconButton
                                         onClick={() => {
+
                                           handleViewFeedback(
                                             company._id,
                                             company["Company Name"],
