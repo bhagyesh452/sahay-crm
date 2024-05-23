506103c1e325dab6d35ef218e0443f2068f1647a
shivangistartupsahay <shivangi@startupsahay.com>
2024-05-23 14:15:20 +0530

    23-05-2024

--- frontend/src/employeeComp/EmployeeDashboardComponents/EmployeeForwardedReport.jsx
+++ frontend/src/employeeComp/EmployeeDashboardComponents/EmployeeForwardedReport.jsx
@@ -155,15 +155,68 @@ function EmployeeForwardedReport() {
   }, [data.ename])
 
   //--------------- function to filter followDataToday -----------------------
+
   const handleFilterFollowDataToday = (isBdm) => {
-    const today = new Date().toISOString().split("T")[0]
-    const filterFollowDataForwarded = isBdm ? followData.filter((company) => company.estPaymentDate === today && company.bdmName !== data.ename && company.caseType === "Forwarded") : followDataToday.filter((company) => company.caseType === "Forwarded")
-    const filterFollowDataRecieved = isBdm ? followDataToday.filter((company) => company.estPaymentDate === today && company.bdmName !== data.ename && company.caseType === "Recieved") : followDataToday.filter((company) => company.caseType === "Recieved")
-    const totalPaymentForwarded = filterFollowDataForwarded.reduce((total, obj) => total + obj.totalPayment, 0)
-    const totalPaymentRecieved = filterFollowDataRecieved.reduce((total, obj) => total + obj.totalPayment / 2, 0)
-    const finalPayment = totalPaymentForwarded + totalPaymentRecieved
-    return finalPayment.toLocaleString();
+    const today = new Date()
+    const currentMonth = today.getMonth();
+    const currentYear = today.getFullYear();
+    if (selectedMonthOption === 'Today') {
+      const filterFollowDataForwarded = isBdm ? followData.filter((company) => 
+        new Date(company.estPaymentDate).getDate() === today.getDate() && company.bdmName !== data.ename && company.caseType === "Forwarded") : followDataToday.filter((company) => company.caseType === "Forwarded")
+      const filterFollowDataRecieved = isBdm ? followDataToday.filter((company) => company.estPaymentDate === today && company.bdmName !== data.ename && company.caseType === "Recieved") : followDataToday.filter((company) => company.caseType === "Recieved")
+      const totalPaymentForwarded = filterFollowDataForwarded.reduce((total, obj) => total + obj.totalPayment, 0)
+      const totalPaymentRecieved = filterFollowDataRecieved.reduce((total, obj) => total + obj.totalPayment / 2, 0)
+      const finalPayment = totalPaymentForwarded + totalPaymentRecieved
+      return finalPayment.toLocaleString();
+
+    } else if (selectedMonthOption === 'This Month') {
+      const filterFollowDataForwarded = isBdm ? followData.filter((company) =>
+        new Date(company.estPaymentDate).getMonth() === currentMonth &&
+        new Date(company.estPaymentDate).getFullYear() === currentYear &&
+        company.bdmName !== data.ename && company.caseType === "Forwarded")
+        : followData.filter((company) =>
+          new Date(company.estPaymentDate).getMonth() === currentMonth &&
+          new Date(company.estPaymentDate).getFullYear() === currentYear &&
+          company.caseType === "Forwarded")
+      const filterFollowDataRecieved = isBdm ? followDataToday.filter((company) =>
+        new Date(company.estPaymentDate).getMonth() === currentMonth &&
+        new Date(company.estPaymentDate).getFullYear() === currentYear &&
+        company.bdmName !== data.ename && company.caseType === "Recieved")
+        : followData.filter((company) =>
+          new Date(company.estPaymentDate).getMonth() === currentMonth &&
+          new Date(company.estPaymentDate).getFullYear() === currentYear &&
+          company.caseType === "Recieved")
+      const totalPaymentForwarded = filterFollowDataForwarded.reduce((total, obj) => total + obj.totalPayment, 0)
+      const totalPaymentRecieved = filterFollowDataRecieved.reduce((total, obj) => total + obj.totalPayment / 2, 0)
+      const finalPayment = totalPaymentForwarded + totalPaymentRecieved
+      return finalPayment.toLocaleString();
+    } else if (selectedMonthOption === 'Last Month') {
+      const lastMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+      const lastMonthYear = lastMonth === 11 ? currentYear - 1 : currentYear;
+      const filterFollowDataForwarded = isBdm ? followData.filter((company) =>
+        new Date(company.estPaymentDate).getMonth() === lastMonth &&
+        new Date(company.estPaymentDate).getFullYear() === lastMonthYear &&
+        company.bdmName !== data.ename && company.caseType === "Forwarded")
+        : followData.filter((company) =>
+          new Date(company.estPaymentDate).getMonth() === lastMonth &&
+          new Date(company.estPaymentDate).getFullYear() === lastMonthYear &&
+          company.caseType === "Forwarded")
+      const filterFollowDataRecieved = isBdm ? followData.filter((company) =>
+        new Date(company.estPaymentDate).getMonth() === lastMonth &&
+        new Date(company.estPaymentDate).getFullYear() === lastMonthYear &&
+        company.estPaymentDate === today && company.bdmName !== data.ename && company.caseType === "Recieved") : followData.filter((company) =>
+          new Date(company.estPaymentDate).getMonth() === lastMonth &&
+          new Date(company.estPaymentDate).getFullYear() === lastMonthYear &&
+          company.caseType === "Recieved")
+      const totalPaymentForwarded = filterFollowDataForwarded.reduce((total, obj) => total + obj.totalPayment, 0)
+      const totalPaymentRecieved = filterFollowDataRecieved.reduce((total, obj) => total + obj.totalPayment / 2, 0)
+      const finalPayment = totalPaymentForwarded + totalPaymentRecieved
+      return finalPayment.toLocaleString();
+    } else {
+      return false;
+    }
   }
+
 
   //---------------function to filter followdata-----------------------------
 
@@ -190,16 +243,43 @@ function EmployeeForwardedReport() {
     return finalPayment.toLocaleString();
   }
 
+  console.log(followData)
+
   //--------------function filter followdatatoday recieved case----------------
   const handleFilterFollowDataTodayRecievedCase = () => {
+    const today = new Date()
+    const currentMonth = today.getMonth();
+    const currentYear = today.getFullYear();
+    if (selectedMonthOption === 'Today') {
+      const filterFollowDataRecieved = followData.filter((company) => 
+        new Date(company.estPaymentDate).getDate() === today.getDate() && company.bdmName === data.ename && company.caseType === "Recieved")
+      console.log(filterFollowDataRecieved)
+      const totalPaymentRecieved = filterFollowDataRecieved.reduce((total, obj) => total + obj.totalPayment / 2, 0)
+      const finalPayment = totalPaymentRecieved
+      return finalPayment.toLocaleString();
 
-    const filterFollowDataRecieved = followDataToday.filter((company) => company.bdmName === data.ename && company.caseType === "Recieved")
-    const totalPaymentRecieved = filterFollowDataRecieved.reduce((total, obj) => total + obj.totalPayment / 2, 0)
-    const finalPayment = totalPaymentRecieved
-    //console.log(finalPayment)
-    //console.log( filterFollowDataRecieved)
+    } else if (selectedMonthOption === 'This Month') {
+      const filterFollowDataRecieved = followData.filter((company) =>
+        new Date(company.estPaymentDate).getMonth() === currentMonth &&
+        new Date(company.estPaymentDate).getFullYear() === currentYear &&
+        company.bdmName === data.ename && company.caseType === "Recieved")
+      const totalPaymentRecieved = filterFollowDataRecieved.reduce((total, obj) => total + obj.totalPayment / 2, 0)
+      const finalPayment = totalPaymentRecieved
+      return finalPayment.toLocaleString();
+    } else if (selectedMonthOption === 'Last Month') {
+      const lastMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+      const lastMonthYear = lastMonth === 11 ? currentYear - 1 : currentYear;
+      const filterFollowDataRecieved = followData.filter((company) =>
+        new Date(company.estPaymentDate).getMonth() === lastMonth &&
+        new Date(company.estPaymentDate).getFullYear() === lastMonthYear &&
+        company.bdmName === data.ename && company.caseType === "Recieved")
+      const totalPaymentRecieved = filterFollowDataRecieved.reduce((total, obj) => total + obj.totalPayment / 2, 0)
+      const finalPayment = totalPaymentRecieved
+      return finalPayment.toLocaleString();
+    }else{
+      return false;
+    }
 
-    return finalPayment.toLocaleString();
   }
   //--------------function to calculate total generated revenue----------------
 
@@ -261,7 +341,8 @@ function EmployeeForwardedReport() {
 
   useEffect(() => {
     filterData(selectedMonthOption);
-  }, [selectedMonthOption, moreEmpData]);
+    filteredTeamData(selectedMonthOption)
+  }, [selectedMonthOption, moreEmpData, teamData]);
 
   const filterData = (period) => {
     let filtered = [];
@@ -273,7 +354,7 @@ function EmployeeForwardedReport() {
       filtered = moreEmpData.filter(
         (obj) => formatDateNow(obj.bdeForwardDate) === today.toISOString().slice(0, 10)
       );
-      
+
     } else if (period === 'This Month') {
       filtered = moreEmpData.filter((obj) => {
         const objDate = new Date(obj.bdeForwardDate);
@@ -315,33 +396,83 @@ function EmployeeForwardedReport() {
     return [];
   };
 
+  //---------------- recieved case function------------------
+  const [selectedOption, setSelectedOption] = useState("Forwarded")
+  const [filteredTeamLeadData, setFilteredTeamLeadData] = useState(teamData)
+
+  const filteredTeamData = (period) => {
+    let filtered = []
+    const today = new Date();
+    const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+
+    if (period === 'Today') {
+      filtered = teamData.filter((obj) => formatDateNow(obj.bdeForwardDate) === today.toISOString().slice(0.10))
+    } else if (period === 'This Month') {
+      filtered = teamData.filter((obj) => new Date(obj.bdeForwardDate) >= firstDayOfMonth &&
+        new Date(obj.bdeForwardDate) <= today)
+    } else if (period === 'Last Month') {
+      const lastDayOfLastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+      filtered = teamData.filter((obj) => new Date(obj.bdeForwardDate) >= lastMonth &&
+        new Date(obj.bdeForwardDate) <= lastDayOfLastMonth)
+    }
+    setFilteredTeamLeadData(filtered)
+  }
+
+  const filteredCategoryDataTeamLeads = (category, period) => {
+    const today = new Date();
+    const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+
+    if (period === 'Today') {
+      return teamData.filter(
+        (obj) => formatDateNow(obj.bdmStatusChangeDate) === today.toISOString().slice(0, 10) && obj.Status === category
+      );
+    } else if (period === 'This Month') {
+      return teamData.filter((obj) => {
+        const objDate = new Date(obj.bdmStatusChangeDate);
+        return objDate >= firstDayOfMonth && objDate <= today && obj.Status === category;
+      });
+    } else if (period === 'Last Month') {
+      const lastDayOfLastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+      return teamData.filter((obj) => {
+        const objDate = new Date(obj.bdmStatusChangeDate);
+        return objDate >= lastMonth && objDate <= lastDayOfLastMonth && obj.Status === category;
+      });
+    }
+
+    return [];
+  }
+
+
 
   return (
     <div>
       <div className="dash-card">
         <div className="dash-card-head d-flex align-items-center justify-content-between">
           <h2 className="m-0">
-            <select class="head-select form-select" id="head-select">
-              <option value="1" selected>Forwarded to BDM</option>
-              <option value="2" disabled={!data.bdmWork} >Received as BDM</option>
+            <select class="head-select form-select" id="head-select" onChange={(e) => setSelectedOption(e.target.value)}>
+              <option value="Forwarded" selected>Forwarded to BDM</option>
+              <option value="Recieved" disabled={!data.bdmWork} >Received as BDM</option>
             </select>
           </h2>
           <div className="dash-select-filter">
             <select class="form-select form-select-sm my-filter-select"
               aria-label=".form-select-sm example"
               value={selectedMonthOption}
-              onChange={(e)=>(
+              onChange={(e) => (
                 setSelectedMonthOption(e.target.value)
                 //handleChange(e.target.value)
               )}
-              >
+            >
               <option value="Today">Today</option>
               <option value="This Month">This Month</option>
               <option value="Last Month">Last Month</option>
             </select>
           </div>
         </div>
-        <div className="dash-card-body">
+        {/* forwraded bdm case report */}
+        {selectedOption === 'Forwarded' && (<div className="dash-card-body">
           <div className="row m-0 align-items-center">
             <div className="col-sm-7 p-0 align-self-stretch h-100">
               <div className="bdm-f-r-revenue h-100">
@@ -429,7 +560,7 @@ function EmployeeForwardedReport() {
                     </div>
                   </div>
                   <div className="call-dr-num clr-e65b5b">
-                  {filterCategoryData('Not Interested', selectedMonthOption).filter((obj) => obj.bdmAcceptStatus === 'Pending' || obj.bdmAcceptStatus === 'Accept').length}
+                    {filterCategoryData('Not Interested', selectedMonthOption).filter((obj) => obj.bdmAcceptStatus === 'Pending' || obj.bdmAcceptStatus === 'Accept').length}
                     {/* {moreEmpData.filter((obj) => formatDateNow(obj.bdmStatusChangeDate) === new Date().toISOString().slice(0, 10) && (obj.bdmAcceptStatus === "Pending" || obj.bdmAcceptStatus === "Accept") && obj.Status === "Not Interested").length} */}
                   </div>
                 </div>
@@ -454,7 +585,113 @@ function EmployeeForwardedReport() {
               </div>
             </div>
           </div>
-        </div>
+        </div>)}
+
+        {/* recieved bdm case report */}
+        {selectedOption === 'Recieved' && (<div className="dash-card-body">
+          <div className="row m-0 align-items-center">
+            <div className="col-sm-7 p-0 align-self-stretch h-100">
+              <div className="bdm-f-r-revenue h-100">
+                <div className="bdm-f-r-revenue-projected">
+                  <div className="roundImggreen">
+                    <div className="roundImggreen-inner-text">
+                      ₹{handleFilterFollowDataTodayRecievedCase()}/-
+                    </div>
+                  </div>
+                  <div className="roundImggreen-text">Projected <br />Revenue</div>
+                </div>
+                <div className="bdm-f-r-revenue-generated">
+                  <div className="roundImgOrg">
+                    <div className="roundImgOrg-inner-text">₹ {functionCalculateGeneratedRevenue(true).toLocaleString()}/-</div>
+                  </div>
+                  <div className="roundImgOrg-text">Generated <br />Revenue</div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-5 p-0 align-self-stretch">
+              <div className="call-dr-names">
+                <div className="call-dr-card d-flex align-items-center justify-content-between pl-0 pt-1 pr-0 pb-1">
+                  <div className="d-flex align-items-center justify-content-between">
+                    <div className="color-squre-dots clr-bg-light-1ac9bd clr-1ac9bd">
+                      <CiViewList />
+                    </div>
+                    <div className="call-dr-name">
+                      General
+                    </div>
+                  </div>
+                  <div className="call-dr-num clr-1ac9bd" >
+                    {filteredTeamLeadData.filter((obj) => obj.bdmStatus === "Untouched").length}
+                  </div>
+                </div>
+                <div className="call-dr-card d-flex align-items-center justify-content-between pl-0 pt-1 pr-0 pb-1">
+                  <div className="d-flex align-items-center justify-content-between">
+                    <div className="color-squre-dots clr-bg-light-ffb900 clr-ffb900">
+                      <MdImportantDevices />
+                    </div>
+                    <div className="call-dr-name">
+                      Interested
+                    </div>
+                  </div>
+                  <div className="call-dr-num clr-ffb900" >
+                    {filteredCategoryDataTeamLeads("Interested", selectedMonthOption).length}
+                  </div>
+                </div>
+                <div className="call-dr-card d-flex align-items-center justify-content-between pl-0 pt-1 pr-0 pb-1">
+                  <div className="d-flex align-items-center justify-content-between">
+                    <div className="color-squre-dots clr-bg-light-4299e1 clr-4299e1">
+                      <LiaAlgolia />
+                    </div>
+                    <div className="call-dr-name">
+                      Follow Up
+                    </div>
+                  </div>
+                  <div className="call-dr-num clr-4299e1">
+                    {filteredCategoryDataTeamLeads("FollowUp", selectedMonthOption).length}
+                  </div>
+                </div>
+                <div className="call-dr-card d-flex align-items-center justify-content-between pl-0 pt-1 pr-0 pb-1">
+                  <div className="d-flex align-items-center justify-content-between">
+                    <div className="color-squre-dots clr-bg-light-1cba19 clr-1cba19">
+                      <LiaClipboardCheckSolid />
+                    </div>
+                    <div className="call-dr-name">
+                      Matured
+                    </div>
+                  </div>
+                  <div className="call-dr-num clr-1cba19">
+                    {filteredCategoryDataTeamLeads("Matured", selectedMonthOption).length}
+                  </div>
+                </div>
+                <div className="call-dr-card d-flex align-items-center justify-content-between pl-0 pt-1 pr-0 pb-1">
+                  <div className="d-flex align-items-center justify-content-between">
+                    <div className="color-squre-dots clr-bg-light-e65b5b clr-e65b5b">
+                      <RiFileCloseLine />
+                    </div>
+                    <div className="call-dr-name">
+                      Not Interested
+                    </div>
+                  </div>
+                  <div className="call-dr-num clr-e65b5b">
+                    {filteredCategoryDataTeamLeads('Not Interested', selectedMonthOption).length}
+                  </div>
+                </div>
+                <div className="call-dr-card d-flex align-items-center justify-content-between pl-0 pt-1 pr-0 pb-1">
+                  <div className="d-flex align-items-center justify-content-between">
+                    <div className="color-squre-dots clr-bg-light-00d19d clr-00d19d">
+                      <IoFileTrayFullOutline />
+                    </div>
+                    <div className="call-dr-name">
+                      Total
+                    </div>
+                  </div>
+                  <div className="call-dr-num clr-00d19d">
+                    {filteredTeamLeadData.length}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>)}
       </div>
     </div>
   )
