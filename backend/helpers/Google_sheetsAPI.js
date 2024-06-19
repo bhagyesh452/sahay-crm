44730691133773301d81fbafd3c8afe15e5c4f9c
bhagyesh452 <bhagyesh@startupsahay.com>
2024-06-19 17:06:54 +0530

    boom

--- backend/helpers/Google_sheetsAPI.js
+++ backend/helpers/Google_sheetsAPI.js
@@ -23,11 +23,13 @@ const auth = new google.auth.JWT(
 const sheets = google.sheets({ version: 'v4', auth });
 const spreadsheetId = process.env.GOOGLE_SPREADSHEET_ID; 
 
+
 function transformData(jsonData) {
   const headers = ["bookingDate", "bookingPublishDate", "Company Name", "Company Email", "Company Number", "panNumber",
     "bdeName", "bdmName", "bookingSource", "numberOfServices", "totalAmount", "receivedAmount", "pendingAmount", "paymentMethod",
     "caCase", "caNumber", "caEmail", "caCommission"
   ];
+
 
   jsonData.services.forEach((serviceObj, index) => {
     headers.push(`services[${index}].serviceName`);
@@ -43,51 +45,31 @@ function transformData(jsonData) {
     if (header.startsWith("services")) {
       const [_, serviceIndex, serviceProp] = header.match(/services\[(\d+)\]\.(.*)/);
       return jsonData.services[serviceIndex][serviceProp] ? jsonData.services[serviceIndex][serviceProp] : "-";
-    } else {
+    }   
+    else {
       return jsonData[header] ? jsonData[header] : "-";
     }
   });
-
-  return data;
+  return [data];
 }
 
-async function getCurrentRowCount() {
+
+async function appendDataToSheet(data) {
   try {
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId,
-      range: 'Sheet1!A:A' // Get the first column to determine the current number of rows
-    });
-    return response.data.values ? response.data.values.length : 0;
-  } catch (error) {
-    console.error('Error fetching row count: ', error);
-    throw error;
-  }
-}
-
-async function appendDataToSheet(jsonData) {
-  try {
-    const currentRowCount = await getCurrentRowCount();
-    const nextIndex = currentRowCount + 1; // Calculate the next index (row count includes header)
-
-    const transformedData = transformData(jsonData);
-    const indexedData = [nextIndex, ...transformedData];
-
+    const transformedData = transformData(data);
+    
     await sheets.spreadsheets.values.append({
       spreadsheetId,
-      range: 'Sheet1!A2', // Start appending data from the second row
+      range: 'Sheet1!A1', 
       valueInputOption: 'RAW',
-      insertDataOption: 'INSERT_ROWS', // Ensure new data inserts rows
       resource: {
-        values: [indexedData],
+        values: transformedData,
       },
     });
-
-    console.log('Data appended successfully.');
   } catch (error) {
     console.error('Error appending data: ', error);
-    throw error; 
+    throw error; // Re-throw error to handle it in the route
   }
 }
 
 module.exports = { appendDataToSheet };
-
