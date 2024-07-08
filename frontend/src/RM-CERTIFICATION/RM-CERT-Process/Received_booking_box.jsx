a97df9e4da1af205a22a74f9ad874c7be72cd523
nimeshparekh <nirmeshparekh1@gmail.com>
2024-07-08 10:24:19 +0530

    add new

---
+++ frontend/src/RM-CERTIFICATION/RM-CERT-Process/Received_booking_box.jsx
@@ -0,0 +1,11 @@
+import React, { useRef, useEffect, useState } from "react";
+
+function EmployeeProfile()
+{
+    return
+    (
+        <div>My Component</div>
+    );
+}
+
+export default ReceivedBookingBox;
