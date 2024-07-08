1d42a7e533d8c63c6e8d9a55cfcdb94fcce0f0ed
Shivangi Agrawal <shivangi@startupsahay.com>
2024-07-08 10:57:50 +0530

    08-07-2024

--- frontend/src/RM-CERTIFICATION/RM-CERT-Process/Received_booking_box.jsx
+++ frontend/src/RM-CERTIFICATION/RM-CERT-Process/Received_booking_box.jsx
@@ -1,11 +1,46 @@
-import React, { useRef, useEffect, useState } from "react";
+import React , {useState , useEffect} from 'react';
+import axios from 'axios';
+import RmofCertificationHeader from "../RM-CERT-COMPONENTS/RmofCertificationHeader";
+import RmCertificationNavbar from "../RM-CERT-COMPONENTS/RmCertificationNavbar";
 
-function EmployeeProfile()
-{
-    return
-    (
-        <div>My Component</div>
-    );
+
+function Received_booking_box() {
+
+
+    const secretKey = process.env.REACT_APP_SECRET_KEY;
+  const [employeeData, setEmployeeData] = useState([])
+
+  const rmCertificationUserId = localStorage.getItem("rmCertificationUserId")
+  console.log(rmCertificationUserId)
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${secretKey}/employee/einfo`);
+      // Set the retrieved data in the state
+      const tempData = response.data;
+      console.log(tempData)
+      const userData = tempData.find((item) => item._id === rmCertificationUserId);
+      console.log(userData)
+      setEmployeeData(userData);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+  console.log('employeeData', employeeData)
+  return (
+    <div>
+       <RmofCertificationHeader name={employeeData.ename} designation={employeeData.designation} />
+       <RmCertificationNavbar rmCertificationUserId={rmCertificationUserId}/>
+       <div>Hello</div>
+    </div>
+  )
 }
 
-export default ReceivedBookingBox;
+export default Received_booking_box
