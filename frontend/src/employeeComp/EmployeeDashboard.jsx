e269ce2555f90c904168fb540cffbc915debfa5e
nimeshparekh <nirmeshparekh1@gmail.com>
2024-07-12 17:42:13 +0530

    add new

--- frontend/src/employeeComp/EmployeeDashboard.jsx
+++ frontend/src/employeeComp/EmployeeDashboard.jsx
@@ -4169,7 +4169,7 @@ function EmployeeDashboard() {
                   <div className="col-sm-4 col-md-4 col-lg-4 mt-3">
                     <EmployeePerformance redesignedData = {redesignedData} data={data}/>
                   </div>
-                  <div className="col-sm-4 col-md-4 col-lg-4 mt-3">
+                  <div className="col-sm-4 col-md-4 col-lg-4 mt-3 Performance_Report_w">
                     <EmployeePerformanceReport redesignedData = {redesignedData} data={data}/>
                     {/* <div className="dash-card">
                       <div className="dash-card-head d-flex align-items-center justify-content-between">
@@ -4261,7 +4261,7 @@ function EmployeeDashboard() {
                   </div>
                   </>}
                   {/* calling data report */}
-                  <div className="col-sm-4 col-md-4 col-lg-4 mt-3">
+                  <div className="col-sm-4 col-md-4 col-lg-4 mt-3 Top_Selling_Services_w">
                     <EmployeeTopSellingServices redesignedData = {redesignedData} ename={data.ename}/>
                   </div>
                   <div className="col-sm-4 col-md-4 col-lg-4 mt-3">
