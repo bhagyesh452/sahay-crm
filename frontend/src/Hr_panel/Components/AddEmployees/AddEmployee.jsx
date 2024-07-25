473d00e921780291de93b17574477d9ccd92e828
nimeshparekh <nirmeshparekh1@gmail.com>
2024-07-25 10:57:03 +0530

    add new

--- frontend/src/Hr_panel/Components/AddEmployees/AddEmployee.jsx
+++ frontend/src/Hr_panel/Components/AddEmployees/AddEmployee.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepButton from "@mui/material/StepButton";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Swal from "sweetalert2";
+import confetti from 'canvas-confetti';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function AddEmployee() {
   return (
     <div>
-      <h1>Add Employee</h1>
+      <h1>Add mnbmb</h1>
     </div>
   )
 }
