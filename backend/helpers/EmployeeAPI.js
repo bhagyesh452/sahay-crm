11334b35eac6f579fd009eba1ebfb6892ebae511
Shivangi Agrawal <shivangi@startupsahay.com>
2024-09-24 14:47:42 +0530

    24-09-2024

--- backend/helpers/EmployeeAPI.js
+++ backend/helpers/EmployeeAPI.js
@@ -2413,9 +2413,11 @@ router.post('/employee-calling/save', async (req, res) => {
   }
 });
 
-router.get(`/employee-calling/:empId`, async (req, res) => {
+router.get(`/employee-calling-fetch/:number`, async (req, res) => {
+  const { number } = req.params; // Accessing the number directly from req.params
+  console.log("number" , number , typeof(number))
   try{
-    const data = await EmployeeCallingModel.find();
+    const data = await CallingModel.find({emp_number : String(number)});
 
     res.status(200).json({ message: 'Data fetched successfully', data: data });
 
