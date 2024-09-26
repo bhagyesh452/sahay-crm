fdfe29c5f482e7d8b6fa3f3d40e07d24e470d3b5
Shivangi Agrawal <shivangi@startupsahay.com>
2024-09-26 16:21:58 +0530

    26-09-2024

--- frontend/src/Hr_panel/Components/EmployeeView/EmloyeeViewAttendance.jsx
+++ frontend/src/Hr_panel/Components/EmployeeView/EmloyeeViewAttendance.jsx
@@ -17,7 +17,20 @@ function EmployeeViewAttendance({ data }) {
     const [lcCount, setLcCount] = useState(0);
     const [attendanceData, setAttendanceData] = useState([]);
     const [months, setMonths] = useState([])
-
+    const monthNamesToNumbers = {
+        "January": 1,
+        "February": 2,
+        "March": 3,
+        "April": 4,
+        "May": 5,
+        "June": 6,
+        "July": 7,
+        "August": 8,
+        "September": 9,
+        "October": 10,
+        "November": 11,
+        "December": 12
+    };
     const convertToDateInputFormat = (selectedDate) => {
         if (!selectedDate) return '';
         const date = new Date(selectedDate);
@@ -26,7 +39,7 @@ function EmployeeViewAttendance({ data }) {
         const day = String(date.getDate()).padStart(2, '0');
         return `${day}-${month}-${year}`;
     };
-
+   
     const handleYearChange = (e) => {
         monthArray(Number(e.target.value));
         setSelectedYear(Number(e.target.value));
@@ -217,20 +230,7 @@ function EmployeeViewAttendance({ data }) {
 
                         return monthNames[monthNumber - 1]; // Subtract 1 since array indices start from 0
                     }
-                    const monthNamesToNumbers = {
-                        "January": 1,
-                        "February": 2,
-                        "March": 3,
-                        "April": 4,
-                        "May": 5,
-                        "June": 6,
-                        "July": 7,
-                        "August": 8,
-                        "September": 9,
-                        "October": 10,
-                        "November": 11,
-                        "December": 12
-                    };
+                    
                     const monthNumber = monthNamesToNumbers[selectedMonth];
                     const formattedDate = `${selectedYear}-${monthNumber < 10 ? '0' + monthNumber : monthNumber}-${date < 10 ? '0' + date : date}`;
                     const isSunday = new Date(`${selectedYear}-${selectedMonth}-${date}`).getDay() === 0;
@@ -448,6 +448,9 @@ function EmployeeViewAttendance({ data }) {
                     </thead>
                     <tbody>
                         {attendanceData.map((emp, index) => {
+                             const attendanceDate = `${selectedYear}-${monthNamesToNumbers[selectedMonth]}-${String(emp.date).padStart(2, '0')}`;
+                            const isSunday = new Date(attendanceDate).getDay() === 0;
+                            console.log("isSunday:", attendanceDate);
                             return (
                                 <tr key={index}>
                                     <td>{index + 1}</td>
@@ -462,7 +465,7 @@ function EmployeeViewAttendance({ data }) {
                                     </td>
                                     <td>
                                         <div className='attendance-date-tbl'>                                          
-                                            <div className="form-control in-time">
+                                            <div className={isSunday ? "" : `form-control in-time`}>
                                                 {(() => {
                                                     console.log('inTime:', emp.inTime); // Console log the inTime value
                                                     return emp.inTime ? convertTo12HourFormat(emp.inTime) : "-"; // Display the inTime value or "-"
@@ -472,7 +475,7 @@ function EmployeeViewAttendance({ data }) {
                                     </td>
                                     <td>
                                         <div className='attendance-date-tbl'>
-                                            <div className="form-cantrol out-time">
+                                            <div className={isSunday ? "" : `form-control out-time`}>
                                                 {emp.outTime ? convertTo12HourFormat(emp.outTime) : "-"}
                                             </div>
                                         </div>
