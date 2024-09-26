52e843c37f94d080253c3dfdc2497ec06b825422
Shivangi Agrawal <shivangi@startupsahay.com>
2024-09-26 16:06:13 +0530

    26-09-2024

--- frontend/src/Hr_panel/Components/EmployeeView/EmloyeeViewAttendance.jsx
+++ frontend/src/Hr_panel/Components/EmployeeView/EmloyeeViewAttendance.jsx
@@ -24,7 +24,7 @@ function EmployeeViewAttendance({ data }) {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
-        return `${year}-${month}-${day}`;
+        return `${day}-${month}-${year}`;
     };
 
     const handleYearChange = (e) => {
