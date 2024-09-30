22ec09f4080a42982742fcab91687d6be4b90cda
Shivangi Agrawal <shivangi@startupsahay.com>
2024-09-30 17:39:05 +0530

    30-09-2024

--- frontend/src/AdminExecutive/ExtraComponents/OtpInboxNo.jsx
+++ frontend/src/AdminExecutive/ExtraComponents/OtpInboxNo.jsx
@@ -1,13 +1,13 @@
 // Token In Box Number
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button, Dialog, DialogContent, DialogTitle, IconButton,DialogActions } from "@mui/material";
+import { Button, Dialog, DialogContent, DialogTitle, IconButton, DialogActions } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import Swal from "sweetalert2";
 
 import { FaPencilAlt } from "react-icons/fa";
 
-const OtpInboxNo = ({ companyName, serviceName, otpInboxNo ,refreshData , mainStatus}) => {
+const OtpInboxNo = ({ companyName, serviceName, otpInboxNo, refreshData, mainStatus }) => {
     const [charges, setCharges] = useState(otpInboxNo);
     const secretKey = process.env.REACT_APP_SECRET_KEY;
     const [openEmailPopup, setOpenEmailPopup] = useState(false);
@@ -34,28 +34,28 @@ const OtpInboxNo = ({ companyName, serviceName, otpInboxNo ,refreshData , mainSt
     };
 
     const handleCloseEmailPopUp = () => {
-              setOpenEmailPopup(false)
+        setOpenEmailPopup(false)
     };
 
     return (
         <div>
-             <div className={'d-flex align-items-center justify-content-between'}>
-            <div
-                className="My_Text_Wrap"
-                title={otpInboxNo}
-            >
-                {otpInboxNo ? otpInboxNo : "Enter Token In Box Number"}
+            <div className={'d-flex align-items-center justify-content-between'}>
+                <div
+                    className="My_Text_Wrap"
+                    title={otpInboxNo}
+                >
+                    {otpInboxNo ? otpInboxNo : "Enter Token In Box Number"}
+                </div>
+                <button className='td_add_remarks_btn ml-1'
+                    onClick={() => {
+
+                        setOpenEmailPopup(true)
+                    }}
+                >
+                    <FaPencilAlt />
+                </button>
             </div>
-            <button className='td_add_remarks_btn ml-1'
-                onClick={() => {
-                   
-                    setOpenEmailPopup(true)
-                }}
-            >
-                <FaPencilAlt/>
-            </button>
-            </div>
-          
+
             <Dialog
                 className='My_Mat_Dialog'
                 open={openEmailPopup}
@@ -64,7 +64,7 @@ const OtpInboxNo = ({ companyName, serviceName, otpInboxNo ,refreshData , mainSt
                 maxWidth="xs"
             >
                 <DialogTitle>
-                <h3 className='m-0'>{companyName}</h3>
+                    <h3 className='m-0'>{companyName}</h3>
                 </DialogTitle>
                 <DialogContent>
                     <div className="card-footer">
@@ -83,13 +83,13 @@ const OtpInboxNo = ({ companyName, serviceName, otpInboxNo ,refreshData , mainSt
                     <Button onClick={handleCloseEmailPopUp}
                         variant="contained"
                         color="error"
-                        style={{ width: "100%",borderRadius:"0px" }} className='m-0'>Close</Button>
-                    
+                        style={{ width: "100%", borderRadius: "0px" }} className='m-0'>Close</Button>
+
                     <Button
                         onClick={handleSubmitPortalCharges}
                         variant="contained"
                         color="primary"
-                        style={{ width: "100%",borderRadius:"0px" }}
+                        style={{ width: "100%", borderRadius: "0px" }}
                         className='m-0'
                     >
                         Submit
