a96f51402bb93220394bf2de32dba75a730d459d
Karan Thakkar <karan.thakkar@startupsahay.com>
2024-10-25 12:40:56 +0530

    also show companies with junk status in team leads

--- backend/helpers/bdmAPI.js
+++ backend/helpers/bdmAPI.js
@@ -347,7 +347,7 @@ router.get("/teamLeadsData/:bdmName", async (req, res) => {
         query["Status"] = "Matured";
         query["bdmAcceptStatus"] = "Accept";
       } else if (status === "Not Interested") {
-        query["Status"] = "Not Interested";
+        query["Status"] = { $in: ["Junk", "Not Interested"] };
         query["bdmAcceptStatus"] = "Accept";
       } else if (status === "General") {
         query["bdmAcceptStatus"] = "Pending";
@@ -397,7 +397,7 @@ router.get("/teamLeadsData/:bdmName", async (req, res) => {
 
     const totalNotInterested = await CompanyModel.countDocuments({
       ...query,
-      Status: "Not Interested",
+      Status: { $in: ["Not Interested", "Junk"] },
       bdmAcceptStatus: "Accept"
     });
 
