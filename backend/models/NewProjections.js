a567e80f1d9df938c5c9d078f10b7969827106b3
Karan Thakkar <karan.thakkar@startupsahay.com>
2024-11-14 14:40:47 +0530

    change api for fetching followup, interested and forwarded count to load data more faster

--- backend/models/NewProjections.js
+++ backend/models/NewProjections.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const CompanySchema = new mongoose.Schema({
   companyName: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   companyId: {
     type: String,
