fab1ee768128be362f97dde1c47d9d9d4d050d9a
Shivangi Agrawal <shivangi@startupsahay.com>
2024-12-10 16:18:08 +0530

    10-12-2024

--- backend/helpers/bookingAPI.js
+++ backend/helpers/bookingAPI.js
@@ -254,7 +254,7 @@ router.get("/redesigned-final-leadData-tableView", async (req, res) => {
           thirdPaymentRemarks: service.thirdPaymentRemarks || "",
           fourthPaymentRemarks: service.fourthPaymentRemarks || "",
           bookingPublishDate: booking.bookingPublishDate || "",
-          pendingRecievedAmount: service.pendingRecievedAmount === 0 ? service.firstPayment : service.pendingRecievedAmount || 0,
+          pendingRecievedAmount: service.pendingRecievedAmount || service.totalPaymentWGST,
           remainingAmount: service.remainingAmount || 0,
         });
       });
@@ -288,7 +288,7 @@ router.get("/redesigned-final-leadData-tableView", async (req, res) => {
             thirdPaymentRemarks: service.thirdPaymentRemarks || "",
             fourthPaymentRemarks: service.fourthPaymentRemarks || "",
             bookingPublishDate: moreBooking.bookingPublishDate || "",
-            pendingRecievedAmount: service.pendingRecievedAmount || 0,
+            pendingRecievedAmount: service.pendingRecievedAmount || service.totalPaymentWGST,
             remainingAmount: service.remainingAmount || 0,
           });
         });
