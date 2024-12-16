e2c93a11872b1c5380a125c4954f4574086b5c88
Shivangi Agrawal <shivangi@startupsahay.com>
2024-12-16 18:18:52 +0530

    16-12-2024

--- frontend/src/admin/AdminUploadQuestions.jsx
+++ frontend/src/admin/AdminUploadQuestions.jsx
@@ -119,7 +119,7 @@ function AdminUploadQuestions() {
 
     // Function to handle Question Click
     const [selectedQuestionFull, setSelectedQuestionFull] = useState("")
-    const handleQuestionClick = async (questionId , questionfull) => {
+    const handleQuestionClick = async (questionId, questionfull) => {
         setEmployeeAnsDialog(true);
         setSelectedQuestion(questionId);
         setSelectedQuestionFull(questionfull);
@@ -128,6 +128,7 @@ function AdminUploadQuestions() {
             const response = await axios.get(
                 `${secretKey}/question_related_api/question-responses/${questionId}`
             );
+            console.log("response.data", response.data)
 
             setEmployeesAnswered(response.data);
         } catch (error) {
@@ -261,7 +262,9 @@ function AdminUploadQuestions() {
                                             return (
                                                 <tr key={index}>
                                                     <td className="rm-sticky-left-1">{index + 1}</td>
-                                                    <td className="rm-sticky-left-2" onClick={() => handleQuestionClick(obj._id , obj.question)}>{obj.question}</td>
+                                                    <td className="rm-sticky-left-2 ellipsis-cell_new" 
+                                                    title={obj.question}
+                                                     onClick={() => handleQuestionClick(obj._id, obj.question)}>{obj.question}</td>
                                                     <td>
                                                         <div className="ellipsis-cell" title={obj.options[0]}>
                                                             {obj.options[0]}
@@ -336,35 +339,54 @@ function AdminUploadQuestions() {
             {/* Popup to Show Question Details */}
             <Dialog open={employeeAnsDialog} onClose={() => setEmployeeAnsDialog(false)} maxWidth="sm" fullWidth className="MY_MAT_DIALOG">
                 <DialogTitle>
-                    <h4>
-                    Employees Responses for Question :
-                    </h4>
-                    <h3>
-                        {selectedQuestionFull}
-                    </h3>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <div className="new_question_dailog_title">
+                            {selectedQuestionFull}
+                        </div>
+                        <div>
+                            <button type="button"
+                                className="btn-close new_question_dailog_button"
+                                aria-label="Close"
+                                onClick={() => setEmployeeAnsDialog(false)}
+                            >
+
+                            </button>
+                        </div>
+                    </div>
+
+
                 </DialogTitle>
+                <hr style={{ border: "1px solid #ddd" , margin:"0" }} />
                 <DialogContent>
                     {employeesAnswered.length > 0 ? (
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Employee Name</th>
-                                    <th>Answer Given</th>
-                                    <th>Is Correct</th>
-                                    <th>Date Answered</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {employeesAnswered.map((employee, index) => (
-                                    <tr key={index}>
-                                        <td>{employee.employeeName}</td>
-                                        <td>{employee.answerGiven}</td>
-                                        <td>{employee.isCorrect}</td>
-                                        <td>{formatDatePro(employee.dateAnswered)}</td>
+                        <div className='table table-responsive table-style-2 m-0'>
+                            <table className="table">
+                                <thead>
+                                    <tr className='tr-sticky'>
+                                        <th>Employee Name</th>
+                                        <th>Answer Given</th>
+                                        <th>Is Correct</th>
+                                        <th>Date Answered</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    {employeesAnswered.map((employee, index) => {
+                                        console.log("employee", employee)
+                                        return (
+                                            <tr key={index}>
+                                                <td>{employee.employeeName}</td>
+                                                <td>{employee.answerGiven}</td>
+                                                <td>{employee.isCorrect === true ? "Correct" : "Incorrect"}</td>
+                                                <td>{formatDatePro(employee.dateAnswered)}</td>
+                                            </tr>
+                                        )
+
+                                    }
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+
                     ) : (
                         <Typography>No responses available for this question.</Typography>
                     )}
